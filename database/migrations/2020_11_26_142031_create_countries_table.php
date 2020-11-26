fda2d6cc77072f82e0b74df9d65d99ad52db5180
Oscar Wales <oscarjwales@gmail.com>
2020-11-26 14:35:24 +0000

    Set up Country model and migration

---
+++ database/migrations/2020_11_26_142031_create_countries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("NAME");
+            $table->string("CONTINENT");
+            $table->string("DISH");
+            $table->string("DESCRIPTION");
+            $table->string("PHOTO");
+            $table->string("RECIPE");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
